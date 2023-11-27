5c8e714541fbc3a32e7f6d60d623691ec304b967
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2023-11-27 14:48:36 +0300

    fix: git cache cleared

--- src/components/modalOverlay/ModalOverlay.tsx
+++
@@ -1,6 +0,0 @@
-import style from './ModalOverlay.module.scss';
-function ModalOverlay(props: { closeModal: () => void }) {
-  return <section className={style.container} onClick={props.closeModal}></section>;
-}
-
-export default ModalOverlay;
